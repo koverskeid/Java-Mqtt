baf3b377a86c55b4d6402bb9c9c13f46746d56ee
koverskeid <koverskeid@gmail.com>
2015-12-03 14:58:55 +0100

    set client id to stationary

--- src/Test.java
+++ src/Test.java
@@ -7,7 +7,7 @@ import org.overskeid.mqtt.service.MqttCommonucationHandler;
 public class Test {
 
 	public static void main(String[] args) {
-		MqttCommonucationHandler communicationHandler = new MqttCommonucationHandler("test.mosquitto.org", 1883, "KristianStationary");
+		MqttCommonucationHandler communicationHandler = new MqttCommonucationHandler("test.mosquitto.org", 1883, "X");
 		new Thread(communicationHandler).start();
 		try {
 			communicationHandler.waitForConnection();
