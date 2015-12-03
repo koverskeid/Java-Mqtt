8ce892f690ac22a36f93ebee484420893a68435d
koverskeid <koverskeid@gmail.com>
2015-12-03 15:33:51 +0100

    Changed client id

--- src/Test.java
+++ src/Test.java
@@ -7,7 +7,7 @@ import org.overskeid.mqtt.service.MqttCommonucationHandler;
 public class Test {
 
 	public static void main(String[] args) {
-		MqttCommonucationHandler communicationHandler = new MqttCommonucationHandler("test.mosquitto.org", 1883, "X");
+		MqttCommonucationHandler communicationHandler = new MqttCommonucationHandler("test.mosquitto.org", 1883, "KristianLaptop1");
 		new Thread(communicationHandler).start();
 		try {
 			communicationHandler.waitForConnection();
